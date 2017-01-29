8c1d85a7cc3bb4a5f70d59d32213070fb3685480
= <=>
2017-01-29 04:05:40 +0100

    Die Morsecode zu Buchstabe Funktion implementieren

--- morsecode.py
+++ morsecode.py
@@ -37,16 +37,34 @@ def wort_zu_morsecode(wort):
 
     print()
 
-def morsecode_zu_wort(morsecode):
-    for wort in morsecode.split(" "):
-        print(wort)
+def morsecode_zu_buchstabe(morsecode):
+    for zeichengruppe in morsecode.split(" "):
+        for buchstabe in morse_alphabet.keys():
+            if zeichengruppe == morse_alphabet[buchstabe]:
+                print(buchstabe, end="")
+
+    print()
+
+def foo():
+    while True:
+        eingabe = input("Morsecode: ")
+
+        if eingabe == "x":
+            break
+        else:
+            morsecode_zu_buchstabe(eingabe)
 
 # Ich will das er mir ganze Sätze in Morsecode übersetzt und umgekeht
+#
 # Format:
+#
 # Hallo .-- . .-.. -
 # .... .- .-.. .-.. --- welt
+#
+# Soll es auch piepsen?
+#
+wort_zu_morsecode("yeah")
+wort_zu_morsecode("ascii")
+wort_zu_morsecode("art")
 
-wort_zu_morsecode("hallo")
-wort_zu_morsecode("welt")
-
-morsecode_zu_wort(".... .- .-.. .-.. ---")
+#foo()

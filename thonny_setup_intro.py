8db9d5ceea43c56bbee4b4fd3e1f2074c378d0e1
Ikem Krueger <ikem.krueger@googlemail.com>
2017-01-30 09:28:26 +0100

    Erster Teil des Intros von Morsecode zu Ascii

---
+++ thonny_setup_intro.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from morsecode import satz_zu_morsecode
+
+print(satz_zu_morsecode("yeah, ascii art would"))
+print(satz_zu_morsecode("have been better but wit"))
+print(satz_zu_morsecode("h variable width font"))
+print(satz_zu_morsecode("it's no good."))

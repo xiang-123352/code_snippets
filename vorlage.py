69289c0f1df90641a5920ce5065f50d46205208b
Ikem Krueger <ikem.krueger@googlemail.com>
2017-02-17 15:43:15 +0100

    Cleanup...

--- vorlage.py
+++ vorlage.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+class Example(object):
+    __init__(self):
+        pass
+    
+    __str__(self):
+        print("Example class...")
+
+if __name__ == "__main__":
+    pass

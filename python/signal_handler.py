a681ee73c95111dbb2393756d5741422d0bef4a1
ikem-krueger <ikem.krueger@googlemail.com>
2017-05-02 13:33:59 +0200

    Update signal_handler.py

--- python/signal_handler.py
+++ python/signal_handler.py
@@ -7,7 +7,9 @@ import time
 
 def sigterm_handler(signal, frame):
     # save the state here or do whatever you want
+    
     print('booyah! bye bye')
+    
     sys.exit(0)
 
 signal.signal(signal.SIGTERM, sigterm_handler)
@@ -15,6 +17,7 @@ signal.signal(signal.SIGTERM, sigterm_handler)
 def main():
     for i in range(100):
         print(i)
+        
         time.sleep(i)
 
 if __name__ == '__main__':

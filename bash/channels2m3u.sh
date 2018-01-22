b4fdf84ac0b42ffe9bc5a65196464f236eb2f780
Ikem Krueger <ikem.krueger@googlemail.com>
2018-01-22 21:25:38 +0100

    Add files via upload

---
+++ bash/channels2m3u.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PLAYLIST=~/DVB-C_playlist.m3u
+SOURCEFILE=~/channels.conf
+
+echo "#EXTM3U" > $PLAYLIST
+
+while read LINE
+do
+
+  name=$(echo $LINE | awk -v FS=":" '{print $1}')
+  programm=$(echo $LINE | awk -v FS=":" '{print $10}')
+  frequency=$(echo $LINE | awk -v FS=":" '{print $2 "000"}')
+  srate=$(echo $LINE | awk -v FS=":" '{print $5 "000"}')
+  modulation=$(echo $(echo $LINE | awk -v FS=":" '{print $3}') | awk -v FS="M" '{print $2}')
+
+  echo "#EXTINF:0,$name" >> $PLAYLIST
+  echo "#EXTVLCOPT:dvb-adapter=0" >> $PLAYLIST
+  echo "#EXTVLCOPT:dvb-frequency=$frequency" >> $PLAYLIST
+  echo "#EXTVLCOPT:dvb-srate=$srate" >> $PLAYLIST
+  echo "#EXTVLCOPT:dvb-modulation=$modulation" >> $PLAYLIST
+  echo "#EXTVLCOPT:program=$programm" >> $PLAYLIST
+  echo "dvb://" >> $PLAYLIST
+
+done<$SOURCEFILE

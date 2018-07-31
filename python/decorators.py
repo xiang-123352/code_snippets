3e63576a6a2f641fd7618c2500526dd5f32f0d0e
Ikem Krueger <ikem.krueger@googlemail.com>
2018-07-31 23:17:53 +0200

    Update decorators.py

--- python/decorators.py
+++ python/decorators.py
@@ -7,17 +7,16 @@ from threading import Thread
 
 def timeit(func):
     def wrapper(*args, **kwargs):
+        # https://realpython.com/primer-on-python-decorators/
         t1 = time.time()
         func(*args, **kwargs)
         t2 = time.time()
         print(">>> Time: " + str((t2 - t1)))
     return wrapper
 
-def thread(func):
+def async(func):
     def wrapper(*args, **kwargs):
-        print(">>> Create thread...")
+        # https://www.saltycrane.com/blog/2008/09/simplistic-python-thread-example/
         t = Thread(target=func, args=args, kwargs=kwargs)
-        
-        print(">>> Start thread...")
         t.start()
     return wrapper
